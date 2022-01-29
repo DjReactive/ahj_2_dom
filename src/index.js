8c5b7538929c0a69c5946edfedc0acea4bb65244
Nikita <dragon768@mail.ru>
2022-01-29 16:02:42 +0500

    Edit 1

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/style.css';
+import img from './images/goblin.png';
+import './js/app';
+
+// TODO: write your code in app.js
---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+const img = document.createElement('img');
+img.setAttribute('src', 'images/goblin.png');
+img.classList.add('image');
+
+const allCells = document.getElementsByClassName('cell');
+
+function getRandomIndex() {
+  return Math.floor(Math.random() * Math.floor(allCells.length));
+}
+
+function setImageOnCell(index) {
+  for (const cell of allCells) {
+    if (allCells[index] === cell) allCells[index].appendChild(img);
+    // removeChild;
+  }
+}
+
+function start() {
+  const rndIndex = getRandomIndex();
+  setImageOnCell(rndIndex);
+}
+
+setInterval(() => start(), 1000);
+start();
